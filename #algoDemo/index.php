0287b3bf25e70aa7aa10e81fa8140a5d3903a796
Elthon Jon Cayetano <Eltonjhoncayetano@gmail.com>
2022-07-08 21:47:03 +0800

    .

---
+++ #algoDemo/index.php
@@ -0,0 +1,84 @@
+<?php
+$customers = array(
+    array ("Jeffrey Canlas", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Elthon Jon Cayetano", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Brian Oniel Galang", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Bhrandel John Perez", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Ilah Zamora", "Pull", "Saturday", 5, "Lourdes Sur", 3)
+
+    array ("Ernesto Avellana", "Push", "Wednesday", 3, "Pandan", 4),
+    array ("Harwin Mendoza", "Push", "Thursday", 2, "Anunas", 7),
+    array ("Mark Lagman", "Pull", "Friday", 4, "Sto. Domingo", 3),
+    array ("Abraham San Pedro", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Alma Cambronero", "Pull", "Saturday", 5, "Malabanias", 3),
+
+    array ("Elaine Paras", "Push", "Wednesday", 3, "Sto. Domingo", 3),
+    array ("Alexa Nicole Ocampo", "Push", "Thursday", 2, "Sto. Domingo", 3),
+    array ("Renz Kieran Perreras", "Pull", "Friday", 4, "Sto. Domingo", 3),
+    array ("Natalie Claire De Guzman", "Pull", "Saturday", 6, "Sapalibutad", 12),
+    array ("Ara Normarie Panganiban", "Pull", "Saturday", 5, "Pulung Cacutud", 12),
+
+    array ("Elcid Peco", "Push", "Wednesday", 3, "Pandan", 4),
+    array ("Ronnie Bituin", "Push", "Thursday", 2, "Anunas", 7),
+    array ("Eugene Perez", "Pull", "Friday", 4, "Sto. Domingo", 3),
+    array ("Maikah Garbes", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Grace Lobo", "Pull", "Saturday", 5, "Malabanias", 3),
+
+    array ("Carlo Lobo", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Maikha Dy", "Push", "Thursday", 2, "Pandan", 4),
+    array ("Trixia Fabian", "Pull", "Friday", 4, "Tabun", 5),
+    array ("Dainielle Rivera", "Pull", "Saturday", 6, "Cutcut", 2),
+    array ("Joan Lovendino", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Francheska Cabero", "Push", "Wednesday", 3, "Pandan", 4),
+    array ("Ronegen Capon", "Push", "Thursday", 2, "Anunas", 7),
+    array ("Carlos Gonzales", "Pull", "Friday", 4, "Sto. Domingo", 3),
+    array ("Grace Penaflor", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Javier Manguerra", "Pull", "Saturday", 5, "Malabanias", 3),
+
+    array ("Cane Halili", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Karla Manalang", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Kimberly Notob", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("matt Daniel Abalos", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Vincent Paul Sigue", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Recto Pangilinan", "Push", "Wednesday", 3, "Pandan", 4),
+    array ("John Rhazid Cadigoy", "Push", "Thursday", 2, "Anunas", 7),
+    array ("Rocky Oberio", "Pull", "Friday", 4, "Sto. Domingo", 3),
+    array ("Kharren Joy Serrano", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Joven Tuazon", "Pull", "Saturday", 5, "Malabanias", 3),
+
+    array ("Jonathan Vital", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Aaron Batac", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Mernita Cristobal", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Kim Matic", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Arthur Dela Pena", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Francisco Olingay", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Jul Newton Legaspi", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Reylen Garcia", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Jene Lianica Arcega", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Israel Miclat", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Reggie Nicol", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Lovely Jane Mercado", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Randy Rosales", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Nelsonjan Perez", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Zeia Beatriz Valencia", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Alma Yu", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Zossimo Oronce", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Jonilo Mababa", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Karen Faye Garcia", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Jae Lobo", "Pull", "Saturday", 5, "Lourdes Sur", 3),
+
+    array ("Jayson De Jesus", "Push", "Wednesday", 3, "Sta. Teresita", 3),
+    array ("Joven Malig", "Push", "Thursday", 2, "Sapangbato", 13),
+    array ("Jervis Lumanlan", "Pull", "Friday", 4, "Lourdes Sur East", 3),
+    array ("Maricar Jose", "Pull", "Saturday", 6, "San Jose", 2),
+    array ("Borgie Quiambao", "Pull", "Saturday", 5, "Lourdes Sur", 3)
+);
+
+
+
+?>
