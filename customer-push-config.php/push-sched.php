da893bd5020e17638b71aad6359752e22cd570f1
Brian Galang <bryangalangoniel@gmail.com>
2022-07-27 23:13:31 +0800

    .

--- customer-push-config.php/push-sched.php
+++ customer-push-config.php/push-sched.php
@@ -537,7 +537,7 @@ require_once "../includes/connect.php";
       <?php include ('./customer-add-alert.php');?>
       <div class="row flex-between-center">
         <div class="col-4 col-sm-auto d-flex align-items-center pe-0">
-          <h5 class="fs-0 mb-0 text-nowrap py-2 py-xl-0">Pull Orders</h5>
+          <h5 class="fs-0 mb-0 text-nowrap py-2 py-xl-0">Push Orders</h5>
         </div>
         <div class="col-4 col-sm-2 d-flex align pe-0 mb-3 ml-2">
           <a href ="./cust_pull_add.php" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addCustomerModal">Add Customer</a>
